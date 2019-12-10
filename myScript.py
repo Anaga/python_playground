df293ce481c4f5f896ecad070b82e40a49300734
Anaga <bobyrev.bob@gmail.com>
2019-12-10 15:59:20 +0200

    I drop some errors here

--- myScript.py
+++ myScript.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 script_version = "1.0.2"
 last_mod_date = "10/12/2019"
@@ -8,8 +8,9 @@ ipo_val = 533
 expec_val = 1500
 
 print("script version is", script_version)
-today = datetime.date.today()
-#today = datetime.date.fromisoformat('2019-12-04')
+#Fix me!
+today = date.today()
+today = date.fromisoformat('2019-12-04')
 
 print("Today is", today.strftime("%d/%m/%Y"))
 next_mounth = today.month + 1;
@@ -23,4 +24,7 @@ delta = (next_month_begin - today).days
 print("Days before next month:", delta)
 print("Enter ammount:")
 ammount = int(input())
+
+if (today.day < pay_day):
+   ammount = ammount - ipo_val	
 print (ammount/delta)
