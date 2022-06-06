f5c1b80de8665208628e2ab347eae43b4808ee06
Aleksandr Boborev <aleksandr.boborev@unikie.com>
2022-06-06 16:32:57 +0300

    add function message_pars Extract unit and value from message row     return dict with:     name - sensor name     text - message text     value - sensor extracted value     unit - sensor Unit of Measurement, extaced from row, is none - set to 'raw'
        Now can parse and extact data from 'sensor', 'hx711' and 'ads1115' moduls.

--- ESP_Home_logs/log_parser.py
+++ ESP_Home_logs/log_parser.py
@@ -41,13 +41,50 @@ def parse_row(line):
         # ['10:26:17', 'C', 'ads1115:077', "     Unit of Measurement: 'Ph'\n"]
         info_list = [] #list()
         for part in parts:
-            short=part[1:]
+            short = part[1:]
             info_list.append(short)
 
-        ret_dict['time']=info_list[0]
-        ret_dict['class']=info_list[1]
-        ret_dict['module']=info_list[2]
-        ret_dict['message']=info_list[3].strip()
+        ret_dict['time'] = info_list[0]
+        ret_dict['class'] = info_list[1]
+        ret_dict['module'] = info_list[2]
+        ret_dict['message'] = info_list[3].strip()
+
+    return ret_dict
+
+def message_pars(message, module):
+    """Extract unit and value from message row
+    return dict with:
+    name - sensor name
+    text - message text
+    value - sensor extracted value
+    unit - sensor Unit of Measurement, extaced from row, is none - set to 'raw'
+    #
+    Now can parse and extact data from 'sensor', 'hx711' and 'ads1115' moduls.
+    """
+    ret_dict = {} #dict()
+    if "':" in message:
+        message_list = message.split("':")
+        dirty_name = message_list[0] # 'TDS Sensor
+        ret_dict['name']= dirty_name[1:] # discard first char
+        ret_dict['text']= message_list[1]
+    else:
+        return ret_dict
+
+    if module=='sensor': #parse " Sending state 6.77216 Ph with 2 decimals of accuracy" text
+        split_list = ret_dict.get('text').split(" ")
+        ret_dict['value']=split_list[3]
+        ret_dict['unit']=split_list[4]
+
+    if module=='hx711': #parse " Got value 556879" text
+        split_list = ret_dict.get('text').split(" ")
+        ret_dict['value']=split_list[3]
+        ret_dict['unit']='raw'
+
+    if module=='ads1115': #parse " Got Voltage=2.047937V" text
+        split_list = ret_dict.get('text').split("=")
+        dirty_value = split_list[1] # '2.047937V'
+        ret_dict['value']=dirty_value[:-1] # '2.047937' - discard last char
+        ret_dict['unit']='V'
 
     return ret_dict
 
@@ -62,6 +99,14 @@ def main():
         time = vals_dict.get('time')
         mod = (vals_dict.get('module').split(":"))[0]
         mess = vals_dict.get('message')
+        data_dict = message_pars(mess, mod)
+        if len(data_dict) == 0: 
+            continue #skip empty dict
+        name = data_dict.get('name')
+        val = data_dict.get('value')
+        unit = data_dict.get('unit')
+        print(F'{time} => {mod} => {name} => {val} {unit}')
+
         if mod not in module_list:
             module_list.append(mod)
             print(F'Got new module {time} => {mod} => {mess}')
