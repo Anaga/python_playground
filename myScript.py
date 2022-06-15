042f7d5be6308bcdc7636f36d3f423b6593d204e
Aleksandr Boborev <aleksandr.boborev@unikie.com>
2022-06-15 11:48:18 +0300

    Refactoring, update function future_spend

--- myScript.py
+++ myScript.py
@@ -1,35 +1,31 @@
+"script to caclulate expected spends and availble day money"
 import datetime
 from datetime import datetime as dt
-from datetime import timedelta
 
 script_version = "1.0.6"
-last_mod_date = "10/06/2022"
+last_mod_date = "15/06/2022"
 
 pay_day = 15
+expec_val = 1000
 
 # 6271516 SEB laenukaitse kindlustuse maksekorraldus -45,89
-ipo_insurense = {"day": 10, "ammount": 45.89, "is_paid": False}
+ipo_insurense = {"day": 10, "ammount": 45.89}
 
 # Lep. 2019009655/intress/L190006408 -220,43
-ipo_intress = {"day": 10, "ammount": 220.43, "is_paid": False}
+ipo_intress = {"day": 10, "ammount": 220.43}
 
 # Lep. 2019009655/põhiosa/L190006408 -312,03
-ipo_pohiosa = {"day": 10, "ammount": 312.03, "is_paid": False}
+ipo_pohiosa = {"day": 10, "ammount": 312.03}
 
 # 102895391 Arve nr A22214516 tasumine -455,26
-lising = {"day": 10, "ammount": 455.26, "is_paid": False}
+lising = {"day": 10, "ammount": 455.26}
 
-life_insurence = {"day": 26, "ammount": 50, "is_paid": False}   #
+life_insurence = {"day": 26, "ammount": 50}   #
 
 expens_array = [ipo_insurense, ipo_intress,
                 ipo_pohiosa, lising, life_insurence]
 
-
-ipo_day = 10
-ipo_val = 1000
-expec_val = 1000
-
-def getDelta(dtToday):
+def getDelta(dtToday, pay_day):
     "function to calculte days count between today and pay day"
     year = dtToday.year
     mounth = dtToday.month
@@ -53,19 +49,16 @@ def future_spend(expens_array, pay_day, today):
 
     for exp in expens_array:
         cur_day = exp.get("day")
-        cur_amm = exp.get("ammount")
+        first_half = cur_day < pay_day
+        futurs_pay = cur_day > today
 
         if today < pay_day:  # first falf of mounth, expected expens is from today till pay_day
-            if cur_day >= today and cur_day < pay_day:
-                future_spends += cur_amm
-
-        """if today == pay_day:  # salary day, expected expens is only today
-            if cur_day == today:
-                future_spends += cur_amm """
+            if first_half and futurs_pay:
+                future_spends += exp.get("ammount")
 
         if today >= pay_day:  # last falf of mounth, expected expens is from today till pay_day in next mounth
-            if cur_day < pay_day or cur_day > today:
-                future_spends += cur_amm
+            if first_half or futurs_pay:
+                future_spends += exp.get("ammount")
 
     return future_spends
 
@@ -75,14 +68,14 @@ today = dt.today()  # get date today
 date = today.strftime("%d/%m/%Y")
 print(F"Today is {date}")
 
-
 print("Enter ammount:")
 ammount = int(input())
+
 will_spend = future_spend(expens_array, pay_day, today.day)
 print(F"will spend: {will_spend}")
 leftOver = ammount - will_spend - expec_val
 
-delta = getDelta(today)
+delta = getDelta(today, pay_day)
 print(F"Days before next pay: {delta}")
 print(F"Left over: {leftOver}")
 print(leftOver/delta)
