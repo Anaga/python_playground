12365c235c46ca82ab25fe15463164b62476d3bb
Anaga <bobyrev.bob@gmail.com>
2023-02-19 23:39:05 +0200

    Update paimenta amounts after euro bore increace

--- myScript.py
+++ myScript.py
@@ -2,32 +2,32 @@
 import datetime
 from datetime import datetime as dt
 
-script_version = "1.0.9"
-last_mod_date = "12/12/2022"
+script_version = "1.0.10"
+last_mod_date = "19/02/2023"
 
 pay_day = 15
 expec_val = 1500
 
 # 6271516 SEB laenukaitse kindlustuse maksekorraldus -45,89
-# ipo_insurense = {"day": 10, "ammount": 45.89}
+ipo_insurense = {"day": 10, "ammount": 48.70}
 
 # Lep. 2019009655/intress/L190006408 -220,43
 # EURIBOR 2.316% 
 # Now percent is 4.216%
 # old percent ipo_intress = {"day": 10, "ammount": 220.43}
-ipo_intress = {"day": 10, "ammount": 497.80}
+ipo_intress = {"day": 10, "ammount": 480.62}
 
 # Lep. 2019009655/põhiosa/L190006408 -312,03
 # old ipo_pohiosa = {"day": 10, "ammount": 312.03}
-ipo_pohiosa = {"day": 10, "ammount": 200.07}
+ipo_pohiosa = {"day": 10, "ammount": 217.25}
 
 # 102895391 Arve nr A22214516 tasumine -455,26
 lising = {"day": 10, "ammount": 462.49}
 
-life_insurence = {"day": 26, "ammount": 50}   #
+life_insurence = {"day": 18, "ammount": 200}   #
 
-#expens_array = [ipo_insurense, ipo_intress, ipo_pohiosa, lising, life_insurence]
-expens_array = [ipo_intress, ipo_pohiosa, lising, life_insurence]
+expens_array = [ipo_insurense, ipo_intress, ipo_pohiosa, lising, life_insurence]
+#expens_array = [ipo_intress, ipo_pohiosa, lising, life_insurence]
 
 def getDelta(dtToday, pay_day):
     "function to calculte days count between today and pay day"
