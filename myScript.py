3745bc8766dbb7acadeebb13a639f56d4e0f87c7
Alexander Boborev <extern.Alexander.Boborev@elektrobit.com>
2023-03-10 10:55:11 +0200

    Update lising and ipoteka costs

--- myScript.py
+++ myScript.py
@@ -2,8 +2,8 @@
 import datetime
 from datetime import datetime as dt
 
-script_version = "1.0.10"
-last_mod_date = "19/02/2023"
+script_version = "1.0.11"
+last_mod_date = "10/03/2023"
 
 pay_day = 15
 expec_val = 1500
@@ -15,14 +15,14 @@ ipo_insurense = {"day": 10, "ammount": 48.70}
 # EURIBOR 2.316% 
 # Now percent is 4.216%
 # old percent ipo_intress = {"day": 10, "ammount": 220.43}
-ipo_intress = {"day": 10, "ammount": 480.62}
+ipo_intress = {"day": 10, "ammount": 433.39}
 
 # Lep. 2019009655/põhiosa/L190006408 -312,03
 # old ipo_pohiosa = {"day": 10, "ammount": 312.03}
-ipo_pohiosa = {"day": 10, "ammount": 217.25}
+ipo_pohiosa = {"day": 10, "ammount": 264.48}
 
 # 102895391 Arve nr A22214516 tasumine -455,26
-lising = {"day": 10, "ammount": 462.49}
+lising = {"day": 10, "ammount": 481.73}
 
 life_insurence = {"day": 18, "ammount": 200}   #
 
